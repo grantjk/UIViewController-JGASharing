e9da0159115653fb7f07ff1f2e54f1486b4227fb
John Grant <johnkgrant@gmail.com>
2012-03-31 13:37:03 -0400

    fix spacing

--- lib/UIViewController+JGASharing.h
+++ lib/UIViewController+JGASharing.h
@@ -13,14 +13,13 @@
 #import <MessageUI/MessageUI.h>
 #import <MessageUI/MFMailComposeViewController.h>
 
-
 @interface UIViewController(JGASharing) <MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 // Display the built in in iOS5 tweet sheet with the provided initial text (optional) and link (optional)
 - (void)displayTweetSheetWithText:(NSString *)text link:(NSString *)link;
 
 // Opens an SMS window with the provided text
--(void)displayTextSheetWithText:(NSString *)text;
+- (void)displayTextSheetWithText:(NSString *)text;
 
 // Opens an email window with the given body and subject
 - (void)displayMailSheetWithBody:(NSString *)body subject:(NSString *)subject;
--- lib/UIViewController+JGASharing.m
+++ lib/UIViewController+JGASharing.m
@@ -12,56 +12,56 @@
 
 - (void)displayTweetSheetWithText:(NSString *)text link:(NSString *)link
 {
-    if ([TWTweetComposeViewController canSendTweet]) {
-        TWTweetComposeViewController *ts = [[TWTweetComposeViewController alloc] init];        
-        [ts setInitialText:text];
-        [ts addURL:[NSURL URLWithString:link]];
-        [self presentViewController:ts animated:YES completion:NULL];
-    }else{
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" 
-                                                        message:@"Please enable Twitter in Settings to use this feature" 
-                                                       delegate:nil 
-                                              cancelButtonTitle:@"Ok" 
-                                              otherButtonTitles: nil];
-        [alert show];
-    }    
+  if ([TWTweetComposeViewController canSendTweet]) {
+    TWTweetComposeViewController *ts = [[TWTweetComposeViewController alloc] init];        
+    [ts setInitialText:text];
+    [ts addURL:[NSURL URLWithString:link]];
+    [self presentViewController:ts animated:YES completion:NULL];
+  }else{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" 
+                                                    message:@"Please enable Twitter in Settings to use this feature" 
+                                                   delegate:nil 
+                                          cancelButtonTitle:@"Ok" 
+                                          otherButtonTitles: nil];
+    [alert show];
+  }    
 }
 - (void)displayTextSheetWithText:(NSString *)text
 {
-    if ([MFMessageComposeViewController canSendText]){
-        MFMessageComposeViewController *vc = [[MFMessageComposeViewController alloc] init];
-        vc.messageComposeDelegate = self;
-        [vc setBody:text];
-        [self presentViewController:vc animated:YES completion:NULL];
-    }else{
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" 
-                                                        message:@"Unable to send SMS from this device." 
-                                                       delegate:nil 
-                                              cancelButtonTitle:@"Ok" 
-                                              otherButtonTitles: nil];
-        [alert show];
-    }
+  if ([MFMessageComposeViewController canSendText]){
+    MFMessageComposeViewController *vc = [[MFMessageComposeViewController alloc] init];
+    vc.messageComposeDelegate = self;
+    [vc setBody:text];
+    [self presentViewController:vc animated:YES completion:NULL];
+  }else{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" 
+                                                    message:@"Unable to send SMS from this device." 
+                                                   delegate:nil 
+                                          cancelButtonTitle:@"Ok" 
+                                          otherButtonTitles: nil];
+    [alert show];
+  }
 }
 - (void)displayMailSheetWithBody:(NSString *)body subject:(NSString *)subject
 {
-	MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
-	picker.mailComposeDelegate = self;
-	[picker setSubject:subject];
-	[picker setMessageBody:body isHTML:NO];
-    [self presentViewController:picker animated:YES completion:NULL];
+  MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
+  picker.mailComposeDelegate = self;
+  [picker setSubject:subject];
+  [picker setMessageBody:body isHTML:NO];
+  [self presentViewController:picker animated:YES completion:NULL];
 }
 
 - (void)mailComposeController:(MFMailComposeViewController*)controller
           didFinishWithResult:(MFMailComposeResult)result
                         error:(NSError*)error 
 {	    
-    [self dismissViewControllerAnimated:YES completion:NULL];
+  [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (void)messageComposeViewController:(MFMessageComposeViewController *)controller
                  didFinishWithResult:(MessageComposeResult)result
 {
-    [self dismissViewControllerAnimated:YES completion:NULL];
+  [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 
